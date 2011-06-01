1158ecd0fd7aad5d000308cff47cc876af86a518
Andy Finnell <andy@fortunatebear.com>
2011-05-31 21:41:17 -0700

    initial commit

---
+++ VectorBoolean/MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  VectorBoolean
+//
+//  Created by Andrew Finnell on 5/31/11.
+//  Copyright 2011 Fortunate Bear, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument {
+@private
+}
+
+@end
---
+++ VectorBoolean/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  VectorBoolean
+//
+//  Created by Andrew Finnell on 5/31/11.
+//  Copyright 2011 Fortunate Bear, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
